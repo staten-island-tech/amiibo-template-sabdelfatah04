0b9b52582ba90d55963d955633ae6f5d17437098
sabdelfatah04 <50589044+sabdelfatah04@users.noreply.github.com>
2019-12-18 11:44:47 -0500

    day3

--- public/index.js
+++ public/index.js
@@ -1,16 +1,22 @@
 const path = require("path");
+const hbs = require("hbs");
 const express = require("express");
 const app = express();
 
 //define paths for express
 const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
 const viewsPath = path.join(__dirname, "../templates/views");
 //setup handlebars engine and views and location
-app.set("views engine", "hbs"); //Telling express/node to use handlebars for our templates
+app.set("view engine", "hbs"); //Telling express/node to use handlebars for our templates
 app.set("views", viewsPath);//telling express to get templates from tamples/views folder
+hbs.registerPartials(partialsPath);
 app.get("", async (req, res) => {
     try {
-      res.render("index"); 
+      res.render("index", {
+          title: "Our First Express App"
+      }); 
     } catch(error) {
         res.status(500).send();
     }
@@ -22,5 +28,24 @@ app.listen(3000, () => {
   console.log("Listening on port 3000");
   console.log(viewsPath);
 });
-
+app.get("/about/:id", async (req, res) => {
+    const name = req.params.id;
+    try {
+      res.render("index", {
+          title: `${name}`
+      }); 
+    } catch(error) {
+        res.status(500).send();
+    }
+}); 
 //http://127.0.0.1:3000/
+app.get("/swag", async (req, res) => {
+    const name = req.params.id;
+    try {
+      res.render("swag", {
+          title: `${name}`
+      }); 
+    } catch(error) {
+        res.status(500).send();
+    }
+});
