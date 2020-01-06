2552d4fe41565f80f2b01497fb7ae7eb9ce35f8e
sabdelfatah04 <50589044+sabdelfatah04@users.noreply.github.com>
2020-01-06 11:27:32 -0500

    done!!

--- src/bundles/amiibo-display.js
+++ src/bundles/amiibo-display.js
@@ -1,3 +1,24 @@
 import {elements} from "./elements";
 const title = document.getElementById('title').textContent;
-console.log(title);
+
+async function displayAmiibo(){
+    const amiiboUrl = `https://www.amiiboapi.com/api/amiibo/?gameseries=${title}`;
+    try {
+        const result = await fetch(amiiboUrl);
+        const data = await result.json();
+        data.amiibo.forEach(element => {
+            elements.displayArea.insertAdjacentHTML(
+              "beforeend",
+              `<ul class="display-amiibo">
+              <li class="display-name">${element.character}</li>
+              <li class="display-series">${element.amiiboSeries}</li>
+              <img class="display-image" src="${element.image}" alt="">
+            </ul>`
+            );
+          });
+    }catch (error) {
+        console.log(error);
+    }
+}
+displayAmiibo();
+//console.log(title);
--- src/bundles/elements.js
+++ src/bundles/elements.js
@@ -1,5 +1,5 @@
 export const elements = {
     amiiboForm: document.getElementById ("amiibo-form"),
     amiiboName: document.getElementById ("amiibo-name"),
-    displayArea: document.getElementById("test")//bad code naming
+    displayArea: document.getElementById ("test")//bad code naming
 };
